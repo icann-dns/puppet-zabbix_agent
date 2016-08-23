32cb991886b31b9f5f2e6e82a61d97743c99e5ec
b4ldr <github@johnbond.org>
2016-08-23 13:34:20 +0100

    remove debug

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,72 +1,5 @@
 # = Class: zabbix_agent
 #
-# This is the main zabbix_agent class
-#
-#
-# == Parameters
-#
-# [*servers*]
-#   Ips of the Zabbix server
-#
-# [*template*]
-#   Sets the path to the template to use as content for main configuration file
-#   If defined, zabbix_agent main config file has: content => content("$template")
-#   Note source and template parameters are mutually exclusive: don't use both
-#   Can be defined also by the (top scope) variable $zabbix_agent_template
-#
-# [*debug*]
-#   Set to 'true' to enable modules debugging
-#   Can be defined also by the (top scope) variables $zabbix_agent_debug and $debug
-#
-# [*noops*]
-#   Set noop metaparameter to true for all the resources managed by the module.
-#   Basically you can run a dryrun for this specific module if you set
-#   this to true. Default: undef
-#
-# Default class params - As defined in zabbix_agent::params.
-# Note that these variables are mostly defined and used in the module itself,
-# overriding the default values might not affected all the involved components.
-# Set and override them only if you know what you're doing.
-# Note also that you can't override/set them via top scope variables.
-#
-# [*package*]
-#   The name of zabbix_agent package
-#
-# [*service*]
-#   The name of zabbix_agent service
-#
-# [*config_file*]
-#   Main configuration file path
-#
-# [*config_file_mode*]
-#   Main configuration file path mode
-#
-# [*config_file_owner*]
-#   Main configuration file path owner
-#
-# [*config_file_group*]
-#   Main configuration file path group
-#
-# [*pid_file*]
-#   Path of pid file. Used by monitor
-#
-# [*log_file*]
-#   Log file(s). Used by puppi
-#
-# [*port*]
-#   The listening port, if any, of the service.
-#   This is used by monitor, firewall and puppi (optional) components
-#   Note: This doesn't necessarily affect the service configuration file
-#   Can be defined also by the (top scope) variable $zabbix_agent_port
-#
-# [*protocol*]
-#   The protocol used by the the service.
-#   This is used by monitor, firewall and puppi (optional) components
-#   Can be defined also by the (top scope) variable $zabbix_agent_protocol
-#
-#
-# See README for usage patterns.
-#
 class zabbix_agent (
   $servers           = $::zabbix_agent::params::servers,
   $start_agents      = $::zabbix_agent::params::start_agents,
@@ -130,11 +63,6 @@ class zabbix_agent (
     noop    => $noops,
     require => Package[$package],
     notify  => Service[$service],
-  }
-
-  ### Debug
-  if $zabbix_agent::bool_debug == true {
-    class { 'zabbix_agent::debug': }
   }
 
 }
